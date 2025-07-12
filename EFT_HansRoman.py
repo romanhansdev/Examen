d90a87a2e49be2126dadd7c39032978d2b1dfeab
Hans Román <romanhansdev@gmail.com>
2025-07-11 20:35:03 -0400

    se sube examen
    examen

---
+++ EFT_HansRoman.py
@@ -0,0 +1,45 @@
+import os
+os.system("cls")
+
+productos = {
+    '8475HD': ['HP', 15.6, '8GB', 'DD', '1T', 'Intel Core i5', 'Nvidia GTX1050'],
+    '2175HD': ['lenovo', 14, '4GB', 'SSD', '512GB', 'Intel Core i5', 'Nvidia GTX1050'],
+    'JjfFHD': ['Asus', 14, '16GB', 'SSD', '256GB', 'Intel Core i7', 'Nvidia RTX2080Ti'],
+    'fgdxFHD': ['HP', 15.6, '8GB', 'DD', '1T', 'Intel Core i3', 'integrada'],
+    'GF75HD': ['Asus', 15.6, '8GB', 'DD', '1T', 'Intel Core i7', 'Nvidia GTX1050'],
+    '123FHD': ['lenovo', 14, '6GB', 'DD', '1T', 'AMD Ryzen 5', 'integrada'],
+    '342FHD': ['lenovo', 15.6, '8GB', 'DD', '1T', 'AMD Ryzen 7', 'Nvidia GTX1050'],
+    'UWU131HD': ['Dell', 15.6, '8GB', 'DD', '1T', 'AMD Ryzen 3', 'Nvidia GTX1050'],
+}
+
+stock = {
+    '8475HD': [387990,10], 
+    '2175HD': [327990,4], 
+    'JjfFHD': [424990,1],
+    'fgdxFHD': [664990,21], 
+    '123FHD': [290890,32], 
+    '342FHD': [444990,7],
+    'GF75HD': [749990,2],
+    'UWU131HD': [349990,1], 
+    'FS1230HD': [249990,0],
+}
+
+def menu ():
+    print("***MENU PRINCIPAL***")
+    print("1. Stock marca")
+    print("2.Busqueda por precio")
+    print("3.Actualizar precio")
+    print("4.Salir")
+
+def stock_marca(marca)
+
+def busque_precio(busque_precio) 
+
+
+def Actualizar_precio(modelo,p)
+
+
+def salir(salir)
+    
+
+
